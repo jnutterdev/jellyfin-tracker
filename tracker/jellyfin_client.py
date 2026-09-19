1fccaeeea64d1a3570fb202ecddd23da2ffe464d
jnutterdev <hello@jnutter.dev>
2026-09-18 21:03:23 -0400

    jellyfin client

--- tracker/jellyfin_client.py
+++ tracker/jellyfin_client.py
@@ -1,5 +1,4 @@
 import json
-import urllib.error
 import urllib.parse
 import urllib.request
 
@@ -9,24 +8,18 @@ url = settings.JELLYFIN_BASE_URL
 token = settings.JELLYFIN_API_KEY
 headers = {"Authorization": f'MediaBrowser Token="{token}"'}
 
-artist_items_param = {
-    "includeItemTypes": "MusicArtist",
-    "recursive": "true",
-    "startIndex": "0",
-    "limit": "100",
-}
 
-get_artists = urllib.parse.urlencode(artist_items_param)
+def fetch_items(include_item_types: str, start_index: int, limit: int) -> dict:
+    fetched_items_object = {
+        "includeItemTypes": include_item_types,
+        "startIndex": start_index,
+        "limit": limit,
+        "recursive": "true",
+    }
+    encoded_items = urllib.parse.urlencode(fetched_items_object)
+    full_url = f"{url}/Items?{encoded_items}"
+    request = urllib.request.Request(full_url, headers=headers)
 
-full_url = f"{url}/Items?{get_artists}"
-
-
-request = urllib.request.Request(full_url, headers=headers)
-
-try:
     with urllib.request.urlopen(request) as response:
-        print("status:", response.status)
         data = json.loads(response.read())
-        print("data:", data)
-except Exception as e:
-    print("ERROR:", type(e).__name__, e)
+        return data
